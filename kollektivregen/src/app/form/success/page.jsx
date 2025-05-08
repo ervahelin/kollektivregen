96c88f4d91a6f793203080f3bd08cec97dcf20cc
ervahelin <ervaguyen@gmail.com>
2025-05-08 19:04:21 +0200

    timeout for successpage

--- kollektivregen/src/app/form/success/page.jsx
+++ kollektivregen/src/app/form/success/page.jsx
@@ -1,6 +1,19 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import Navigation from "../../../components/navigation";
 
 const SuccessPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/dashboard");
+    }, 2000);
+
+    return () => clearTimeout(timer); 
+  }, [router]);
+
   return (
     <div>
       <div className="flex flex-col items-center justify-center min-h-screen p-10 text-center">
